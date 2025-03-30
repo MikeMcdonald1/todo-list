011e1fa827644bd373a2345f792abf6f232c00ef
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-03-30 15:15:06 -0400

    week-3 add state for new todo input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+import { useState } from 'react';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('ok');
   return (
     <div>
       <h1>To Do List</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
