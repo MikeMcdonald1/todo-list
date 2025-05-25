b6871db0609d42b3e705b4a0c2b84220e042dc7a
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-05-25 15:59:03 -0400

    Defined reducer function with switch statement to handle all action types

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -1,3 +1,5 @@
+import { useReducer } from 'react';
+
 const actions = {
   //actions in useEffect that loads todos
   fetchTodos: 'fetchTodos',
@@ -16,6 +18,53 @@ const actions = {
   //action on Dismiss Error button
   clearError: 'clearError',
 };
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actions.fetchTodos:
+      return {
+        ...state,
+      };
+    case actions.loadTodos:
+      return {
+        ...state,
+      };
+    case actions.setLoadError:
+      return {
+        ...state,
+      };
+    case actions.startRequest:
+      return {
+        ...state,
+      };
+    case actions.addTodo:
+      return {
+        ...state,
+      };
+    case actions.endRequest:
+      return {
+        ...state,
+      };
+    case actions.updateTodo:
+      return {
+        ...state,
+      };
+    case actions.completeTodo:
+      return {
+        ...state,
+      };
+    case actions.revertTodo:
+      return {
+        ...state,
+      };
+    case actions.clearError:
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+}
 
 const initialState = {
   todoList: [],
